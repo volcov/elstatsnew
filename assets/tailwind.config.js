3966f23dc9bc17beae00cfcb41c5dc5bd1616ba9
volcov <volcov.bruno@gmail.com>
2021-02-23 21:59:09 -0300

    purge unused styles in production

---
+++ assets/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['../lib/**/*.ex',
+    '../lib/**/*.leex',
+    '../lib/**/*.eex',
+    './js/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
